164a2f6b8fa488637be91463d8f50d5266dee487
João Martiniano <44736477+joaomartiniano@users.noreply.github.com>
2024-03-04 20:19:15 +0000

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                No concurso Euromilhões estas foram as últimas chaves sorteadas:
+                    4 7 19 20 34    + 2 4
+                    3 4 9 12 20     + 5 6
+                    24 27 28 30 49  + 1 12
+                    23 31 37 42 48  + 3 7
+                    8 13 14 24 26   + 1 2
+                    13 17 18 20 46  + 4 9
+                    23 24 35 37 45  + 9 12
+
+                (os dois últimos números são as estrelas)
+
+                Armazene as chaves num array bidimensional.
+            */
+
+            // *** Inserir código C# aqui
+
+
+            /*
+                Mostre as chaves na consola, no seguinte formato:
+                    Chave do sorteio: números + estrela
+
+                Exemplo:
+                    Chave do sorteio: 4  7  19  20  34  +  2  4
+            */
+
+            // *** Inserir código C# aqui
+
+
+            Console.ReadKey();
+        }
+    }
+}
